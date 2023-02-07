545e6b188858d5c51a14e4a11a61be089360b22e
tikhomirov-ruslan <116636276+tikhomirov-ruslan@users.noreply.github.com>
2023-02-07 14:20:43 +0600

    Add files via upload

---
+++ TSIS1/Casting.py
@@ -0,0 +1,22 @@
+x = int(1)   # x will be 1 
+print(x)
+y = int(2.8) # y will be 2
+print(y)
+z = int("3") # z will be 3
+print(z)
+
+x = float(1)     # x will be 1.0
+print(x)
+y = float(2.8)   # y will be 2.8
+print(y)
+z = float("3")   # z will be 3.0
+print(z)
+w = float("4.2") # w will be 4.2
+print(w)
+
+x = str("s1") # x will be 's1'
+print(x)
+y = str(2)    # y will be '2'
+print(y)
+z = str(3.0)  # z will be '3.0'
+print(z)
