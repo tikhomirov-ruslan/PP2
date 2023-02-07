026724cd704b950a32908c355a9b731bd32aa588
tikhomirov-ruslan <116636276+tikhomirov-ruslan@users.noreply.github.com>
2023-02-07 16:48:32 +0600

    Add files via upload

---
+++ TSIS2/Dictionaries.py
@@ -0,0 +1,53 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+# Dictionary Items
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+# Duplicates Not Allowed
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+# Dictionary Length
+print(len(thisdict))
+
+# Dictionary Items - Data Types
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+# type()
+# <class 'dict'> 
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+# The dict() Constructor
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
