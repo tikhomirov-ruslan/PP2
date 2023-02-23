7fe4f744bfb76f848149c6fbdb4b90c5c4071a78
tikhomirov-ruslan <116636276+tikhomirov-ruslan@users.noreply.github.com>
2023-02-23 15:01:03 +0600

    Add files via upload

--- TSIS5/RegEx_1.py
+++ TSIS5/RegEx_1.py
@@ -1,16 +1,14 @@
 # 1
 import re
-txt = str(input())
-a = re.findall("ab", txt)
-b = re.findall("a0", txt)
-c = re.findall("abb", txt)
-print(a, b, c)
+txt = input()
+ans = re.findall("ab*", txt)
+print(ans)
 # 2
 x = re.findall ("ab{2,3}", txt)
 print(x)
 
 # 3
-x = re.findall("[a-z][_][a-z]", txt)
+x = re.findall("[a-z]+_", txt)
 print(x)
 
 # 4
@@ -18,9 +16,26 @@ x = re.findall("[A-Z][a-z]+", txt)
 print(x)
 
 # 5
-x = re.findall("a.+b$", txt)
+x = re.findall("a.*b$", txt)
 print(x)
 
 # 6 
-x = re.sub("[ ,.]","|", txt)
+x = re.sub("[ ,.]", ":", txt)
 print(x)
+
+# 7
+myTxt = "helloWorld"
+my_txt = "hello_world"
+x = re.findall(r"_[a-z]", my_txt)[0][1]
+print(x)
+y = re.sub(r"_[a-z]", x.upper(), my_txt)
+print(y)
+
+# 8
+
+
+# 9
+x = re.findall(r"[A-Z]", myTxt)
+print(x)
+y = re.sub(r" ")
+# 10
