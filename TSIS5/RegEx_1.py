4bc40412c7a412fbb915ab893fe57bf73f695c92
tikhomirov-ruslan <116636276+tikhomirov-ruslan@users.noreply.github.com>
2023-03-02 15:55:03 +0600

    Add files via upload

--- TSIS5/RegEx_1.py
+++ TSIS5/RegEx_1.py
@@ -2,40 +2,47 @@
 import re
 txt = input()
 ans = re.findall("ab*", txt)
-print(ans)
+#print(ans)
 # 2
 x = re.findall ("ab{2,3}", txt)
-print(x)
+#print(x)
 
 # 3
 x = re.findall("[a-z]+_", txt)
-print(x)
+#print(x)
 
 # 4
 x = re.findall("[A-Z][a-z]+", txt)
-print(x)
+#print(x)
 
 # 5
 x = re.findall("a.*b$", txt)
-print(x)
+#print(x)
 
 # 6 
 x = re.sub("[ ,.]", ":", txt)
-print(x)
+#print(x)
 
 # 7
 myTxt = "helloWorld"
 my_txt = "hello_world"
 x = re.findall(r"_[a-z]", my_txt)[0][1]
-print(x)
+#print(x)
 y = re.sub(r"_[a-z]", x.upper(), my_txt)
-print(y)
+#print(y)
 
 # 8
 
 
 # 9
 x = re.findall(r"[A-Z]", myTxt)
-print(x)
-y = re.sub(r" ")
+#print(x)
+#y = re.sub(r" ")
+
 # 10
+myTxt = "helloWorld"
+my_txt = "hello_world"
+x = re.findall(r"[A-Z]", myTxt)[0]
+#print(x)
+y = re.sub(r"[A-Z]", "_w", myTxt)
+#print(y)
